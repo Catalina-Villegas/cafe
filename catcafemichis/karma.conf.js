e1c3f2ce755b95ba5c1dd59dfe4be60b57b501d2
Catalina-Villegas <cat.villegasm@duocuc.cl>
2025-11-13 19:49:23 -0300

    Update karma.conf.js

--- catcafemichis/karma.conf.js
+++ catcafemichis/karma.conf.js
@@ -1,24 +1,64 @@
+const path = require("path");
+
 const webpackConfig = {
   mode: "development",
   module: {
     rules: [
-      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            // Asegúrate de tener estos presets instalados
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
-      { test: /\.(svg|png|jpg|jpeg|gif)$/, use: "file-loader" }
-    ],
+      { test: /\.(svg|png|jpg|jpeg|gif)$/, type: "asset/resource" }
+    ]
   },
-  resolve: { extensions: [".js", ".jsx"] },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src") // opcional, para rutas cortas tipo "@/components"
+    }
+  },
+  devtool: "inline-source-map"
 };
 
 module.exports = function (config) {
   config.set({
     basePath: "",
     frameworks: ["jasmine"],
+
+    // Todos los archivos de test
     files: ["test/**/*.spec.js"],
-    preprocessors: { "test/**/*.spec.js": ["webpack"] },
+
+    preprocessors: {
+      "test/**/*.spec.js": ["webpack", "sourcemap"]
+    },
+
     webpack: webpackConfig,
-    browsers: ["ChromeHeadless"],
-    singleRun: true,
+
+    // Reportes de salida
     reporters: ["progress"],
+
+    // Usa ChromeHeadless sin entorno gráfico (perfecto para EC2)
+    browsers: ["ChromeHeadlessNoSandbox"],
+
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox", "--disable-gpu", "--disable-dev-shm-usage"]
+      }
+    },
+
+    singleRun: true,
+    concurrency: Infinity,
+
+    // Opcional: tiempo extra por si los tests demoran más en EC2
+    browserNoActivityTimeout: 60000
   });
 };
